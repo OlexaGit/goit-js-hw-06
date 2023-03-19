155ef12678c1127db706373e706109d20c7d1930
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-03-19 14:15:04 +0200

    HW_js_task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,50 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// ---version № 1---
+
+// const totalArray = [];
+// const addIngredients = ingredients.forEach((item) => {
+//   const itemEl = document.createElement("li");
+//   itemEl.textContent = item;
+//   itemEl.classList.add("item");
+//   totalArray.push(itemEl);
+//   console.log(itemEl);
+// });
+// console.log(totalArray);
+// const ingredientsElementAll = document.querySelector("#ingredients");
+// ingredientsElementAll.append(...totalArray);
+
+// ---version № 2---
+
+// const totalArray = [];
+// const addIngredients = ingredients.map((item) => {
+//   const itemEl = document.createElement("li");
+//   itemEl.textContent = item;
+//   itemEl.classList.add("item");
+//   totalArray.push(itemEl);
+//   console.log(itemEl);
+// });
+// console.log(totalArray);
+// const ingredientsElementAll = document.querySelector("#ingredients");
+// ingredientsElementAll.append(...totalArray);
+
+// ---version № 3---
+
+const addIngredients = ingredients.reduce((total, elem) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = elem;
+  itemEl.classList.add("item");
+  total.push(itemEl);
+  console.log(itemEl);
+  return total;
+}, []);
+console.log(addIngredients);
+const ingredientsElementAll = document.querySelector("#ingredients");
+ingredientsElementAll.append(...addIngredients);
