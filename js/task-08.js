49114372976abc1e56fdc58e1a83fadfc9873dd6
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-03-20 15:15:24 +0200

    HW_task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (email.length === 0 || password.length === 0) {
+    alert("Please fill in all fields");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+  }
+  formEl.reset();
+}
