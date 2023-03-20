e670469b26a480060d3726b942e1bc32903de635
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-03-21 01:00:57 +0200

    HW_task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,47 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector("input"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  boxesEl: document.querySelector("#boxes"),
+};
+
+const totalArray = [];
+
+refs.inputEl.addEventListener("input", enterInput);
+
+function enterInput(event) {
+  let amount = event.currentTarget.value;
+  refs.createBtn.addEventListener("click", () => {
+    createBoxes(amount);
+    appElementHtml(amount);
+  });
+
+  refs.destroyBtn.addEventListener("click", () => {
+    refs.boxesEl.innerHTML = "";
+    totalArray.splice(0, amount);
+    amount = 0;
+    // refs.inputEl.data = null;
+    // event.currentTarget.reset();
+    // event.currentTarget.value = "";
+  });
+}
+
+function appElementHtml() {
+  refs.boxesEl.append(...totalArray);
+}
+
+function createBoxes(amount) {
+  let stepPx = 0;
+  for (let i = 0; i < amount; i++) {
+    const divEl = document.createElement("div");
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.style.width = 30 + stepPx + "px";
+    divEl.style.height = 30 + stepPx + "px";
+    stepPx += 10;
+    totalArray.push(divEl);
+  }
+}
