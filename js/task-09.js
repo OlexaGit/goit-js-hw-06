55307b1032cb2d0acc5b2812de97a52a9e853198
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-03-20 16:22:32 +0200

    HW_task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const backgroundEl = document.querySelector("body");
+const valueColorEl = document.querySelector(".color");
+const targetBtnEl = document.querySelector(".change-color");
+
+targetBtnEl.addEventListener("click", () => {
+  let valueColor = getRandomHexColor();
+  valueColorEl.textContent = valueColor;
+  backgroundEl.style.backgroundColor = valueColor;
+});
